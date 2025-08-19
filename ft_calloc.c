f7f4b5e23b9136e9abd7979459120643a7bd2342
omar alethamat <omaralethamat@gmail.com>
2025-08-19 14:46:21 +0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,12 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 	size_t	total;
 
+	if (size && count > SIZE_MAX / size)
+		return (NULL);
 	total = count * size;
 	ptr = malloc(total);
 	if (!ptr)
