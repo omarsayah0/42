7e85df0daa68ef65cd3b1586d1820d6cb4d3fe78
omar alethamat <omaralethamat@gmail.com>
2025-08-19 15:16:03 +0300

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -12,10 +12,7 @@
 
 int	ft_isalpha(int c)
 {
-	unsigned char	ch;
-
-	ch = c;
-	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
 	return (0);
 }
