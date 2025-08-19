fb3211710b2de048b84342f2b61762c93577d44b
omar alethamat <omaralethamat@gmail.com>
2025-08-19 15:15:31 +0300

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -13,12 +13,9 @@
 
 int	ft_isalnum(int c)
 {
-	unsigned char	ch;
-
-	ch = c;
-	if ((ch >= 'A' && ch <= 'Z')
-		|| (ch >= 'a' && ch <= 'z')
-		|| (ch >= '0' && ch <= '9'))
+	if ((c >= 'A' && c <= 'Z')
+		|| (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
 		return (1);
 	return (0);
 }
